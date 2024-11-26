900caf049e3caaa33dc6a685a850e2a9b856a483
lezajas <slezaja@veleri.hr>
2024-11-26 01:53:20 +0100

    popravljanje 4.og edita

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,15 +1,15 @@
 <template>
   <q-page class="flex flex-center">
-<<<<<<< HEAD:Qarsar/quasar-project/src/pages/IndexPage.vue
-    <img alt="Knjiznica slika" src="~assets/knjiznica_slika.jpg" style="width: 100%; height: 100%">
-=======
-    <img alt="Knjiznica slika" src="~assets/knjiznica_slika.jpg" style="width: 1000px; height: 1000px">
->>>>>>> ed0e16bf341dc654c2e7d10f2c7713797e095312:quasar-project/src/pages/IndexPage.vue
+    <img
+      alt="Knjiznica slika"
+      src="~assets/knjiznica_slika.jpg"
+      style="width: 100%; height: 100%"
+    />
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
--- quasar-project/src/pages/ONamaage.vue
+++ quasar-project/src/pages/ONamaage.vue
@@ -7,38 +7,28 @@
   <q-page padding>
     <q-card dark bordered class="bg-grey-9 my-card">
       <q-card-section class="text-h4 text-justify">
-<<<<<<< HEAD:Qarsar/quasar-project/src/pages/ONamaage.vue
-        <div class="text-h3 text-weight-bolder absolute-top text-center">O
-          nama</div>
+        <div class="text-h3 text-weight-bolder absolute-top text-center">
+          O nama
+        </div>
       </q-card-section>
-      <q-card-section class="text-body1 ">
+      <q-card-section class="text-body1">
         {{ o_nama }}
-        <q-img src=" https://www.rijeka.hr/wp-content/uploads/2023/12/Nova-knji%C5%BEnica-4-901x600.jpg ">
-=======
-        {{ o_nama }}
-      </q-card-section>
-      <q-card-section>
-        <q-img src=" https://www.rijeka.hr/wp-content/uploads/2023/12/Nova-knji%C5%BEnica-4-901x600.jpg ">
-          <div class="text-h3 text-weight-bolder absolute-bottom text-center">O
-            nama</div>
->>>>>>> ed0e16bf341dc654c2e7d10f2c7713797e095312:quasar-project/src/pages/ONamaage.vue
+        <q-img
+          src=" https://www.rijeka.hr/wp-content/uploads/2023/12/Nova-knji%C5%BEnica-4-901x600.jpg "
+        >
         </q-img>
       </q-card-section>
     </q-card>
   </q-page>
-
 </template>
 
 <script>
 export default {
   setup() {
     return {
-<<<<<<< HEAD:Qarsar/quasar-project/src/pages/ONamaage.vue
-      o_nama: "Gradska knjižnica Rijeka središnja je narodna knjižnica grada Rijeke i matična knjižnica za narodne i školske knjižnice Primorsko-goranske županije. Ona je informacijsko, obrazovno, kulturno, komunikacijsko i socijalno središte Rijeke i okolice usmjereno prema novom stvaralaštvu, održivom razvoju i kvaliteti razvoja zajednice i čitateljske kulture."
-=======
-      o_nama: "'Gradska knjižnica Rijeka središnja je narodna knjižnica grada Rijeke i matična knjižnica za narodne i školske knjižnice Primorsko-goranske županije. Ona je informacijsko, obrazovno, kulturno, komunikacijsko i socijalno središte Rijeke i okolice usmjereno prema novom stvaralaštvu, održivom razvoju i kvaliteti razvoja zajednice i čitateljske kulture."
->>>>>>> ed0e16bf341dc654c2e7d10f2c7713797e095312:quasar-project/src/pages/ONamaage.vue
-    }
-  }
-}
+      o_nama:
+        "Gradska knjižnica Rijeka središnja je narodna knjižnica grada Rijeke i matična knjižnica za narodne i školske knjižnice Primorsko-goranske županije. Ona je informacijsko, obrazovno, kulturno, komunikacijsko i socijalno središte Rijeke i okolice usmjereno prema novom stvaralaštvu, održivom razvoju i kvaliteti razvoja zajednice i čitateljske kulture.",
+    };
+  },
+};
 </script>
